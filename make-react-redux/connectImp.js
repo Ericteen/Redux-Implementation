ad168f09894052504a7e01674f9d294d3818da92
Ericteen <chengteen@yeah.net>
2017-09-30 11:54:14 +0800

    connect 高阶函数模拟

--- make-react-redux/connectImp.js
+++ make-react-redux/connectImp.js
@@ -29,3 +29,27 @@ const mapDispatchToProps = ({ dispatch }) => {
     }
   }
 }
+
+export /**
+ * 
+ * @desc  connect 高阶函数模拟
+ *        利用 context 特性 取到 store
+ *        用 getChildContext() 将 store 放到 Index 的 context 里面
+ *        这样每个组件就能获取到 store 了
+ * @param {function} mapStateToProps 
+ * @param {function} mapDispatchToProps 
+ */
+const connect = (mapStateToProps, mapDispatchToProps) => (WrappedComponent) => {
+  class Connect extends Component {
+    static contextTypes = {
+      store: PropTypes.object,
+    }
+
+    render() {
+      const {store} = this.context
+      let stateProps = mapStateToProps(store.getState())
+      let dispatchProps = mapDispatchToProps(store.dispatch)
+      return <WrappedComponent {...stateProps, ...dispatchProps} />
+    }
+  } 
+}
